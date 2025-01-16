e2d2e71982610549e7ffd0924307de057f6a4b90
Shyamala9926 <shyamlajanamoni2002@gmail.com>
2025-01-16 14:52:13 +0530

    adding repo-url

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/my-portfolio',
+  base:'/My-portfolio',
   plugins: [react()],
   css: {
     modules: {
